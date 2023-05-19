114b782e29bf5c55c3563c4e90dc45d3209a4384
murtuzaprogram <murtuzaprogram@gmail.com>
2023-05-19 17:21:52 +0530

    Done changes

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ df=pd.read_csv('diabetes.csv')
 X=df.iloc[:,:-1]
 Y=df.iloc[:,-1]
 
-X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.3,random_state=0)
+X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.25,random_state=0)
 
 classifier=RandomForestClassifier()
 classifier.fit(X_train,y_train)
